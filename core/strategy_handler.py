d4867c79bd42bbf3f80b8a5cca4dd29adf4819ca
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-08-01 12:45:59 +0200

    Use strategy_handler.py to apply strategies to any Class-, Method- or FieldAnalysis Objects

---
+++ core/strategy_handler.py
@@ -0,0 +1,32 @@
+FLAG_CLASS = 0b1
+FLAG_METHOD = 0b10
+FLAG_FIELD = 0b100
+
+
+class StrategyHandler:
+    def __init__(self):
+        self.strategies = []
+        self.new_idx = 0
+
+    def add_strategy(self, apply_strategy, flags):
+        self.new_idx = len(self.strategies)
+        self.strategies.append((flags, apply_strategy))
+
+    def invoke_strategies(self, r_cas=None, r_mas=None, r_fas=None, only_new=False):
+        flags = 0
+        if r_cas:
+            flags |= FLAG_CLASS
+        if r_mas:
+            flags |= FLAG_METHOD
+        if r_fas:
+            flags |= FLAG_FIELD
+
+        if only_new:
+            s_idx = self.new_idx
+        else:
+            s_idx = 0
+        for idx, (flag, strategy) in enumerate(self.strategies):
+            if (s_idx > idx) or (flags & flag == 0):
+                continue
+
+            strategy(r_cas, r_mas, r_fas)
